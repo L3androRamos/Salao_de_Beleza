45261c658f8b734815535529b2da0e3c9ab02dd6
Beatriz <nanamlniiya@gmail.com>
2023-09-28 20:39:12 -0300

    getter setter

--- execute/src/modelo/Cliente.java
+++ execute/src/modelo/Cliente.java
@@ -4,4 +4,24 @@ public class Cliente {
     private String nome;
     private int numeroDeContato;
 
+    public Cliente(String nome, int numeroDeContato) {
+        this.nome = nome;
+        this.numeroDeContato = numeroDeContato;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroDeContato() {
+        return numeroDeContato;
+    }
+
+    public void setNumeroDeContato(int numeroDeContato) {
+        this.numeroDeContato = numeroDeContato;
+    }
 }
--- execute/src/modelo/Servico.java
+++ execute/src/modelo/Servico.java
@@ -5,4 +5,25 @@ import java.util.Date;
 public class Servico {
     private String tipoServico;
     private Date dataMarcada;
+
+    public Servico(String tipoServico, Date dataMarcada) {
+        this.tipoServico = tipoServico;
+        this.dataMarcada = dataMarcada;
+    }
+
+    public String getTipoServico() {
+        return tipoServico;
+    }
+
+    public void setTipoServico(String tipoServico) {
+        this.tipoServico = tipoServico;
+    }
+
+    public Date getDataMarcada() {
+        return dataMarcada;
+    }
+
+    public void setDataMarcada(Date dataMarcada) {
+        this.dataMarcada = dataMarcada;
+    }
 }
